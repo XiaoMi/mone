b9e351c33a68224d0c840d1a44516546b5d41a6c
goodjava <goodjava@163.com>
2021-02-24 14:47:24 +0800

    [feat][mesh] 支持grpc

---
+++ jcommon/grpc/src/main/java/com/xiaomi/mone/grpc/GrpcClient.java
@@ -0,0 +1,97 @@
+package com.xiaomi.mone.grpc;
+
+import com.xiaomi.mone.grpc.demo.*;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author goodjava@qq.com
+ * @date 1/2/21
+ */
+@Slf4j
+public class GrpcClient {
+
+    private ConcurrentHashMap<String, ManagedChannel> channelMap = new ConcurrentHashMap<>();
+
+    /**
+     * 发送消息到服务端
+     *
+     * @param address
+     * @param request
+     * @return
+     */
+    public GrpcMeshResponse call(MeshAddress address, GrpcMeshRequest request) {
+        String key = address.getIp() + ":" + address.getPort();
+        ManagedChannel ch = channelMap.compute(key, (k, v) -> {
+            if (null == v) {
+                ManagedChannel channel = ManagedChannelBuilder.forAddress(address.getIp(), address.getPort())
+                        .usePlaintext()
+                        .build();
+                return channel;
+            } else {
+                return v;
+            }
+        });
+        MeshServiceGrpc.MeshServiceBlockingStub stub = MeshServiceGrpc.newBlockingStub(ch);
+        GrpcMeshResponse response = stub.call(request);
+        return response;
+    }
+
+    /**
+     * 监听server推回来的信息
+     *
+     * @param address
+     * @param request
+     * @throws InterruptedException
+     */
+    public void listen(MeshAddress address, GrpcMeshRequest request) throws InterruptedException {
+        String key = address.getIp() + ":" + address.getPort();
+        ManagedChannel ch = channelMap.compute(key, (k, v) -> {
+            if (null == v) {
+                ManagedChannel channel = ManagedChannelBuilder.forAddress(address.getIp(), address.getPort())
+                        .usePlaintext()
+                        .build();
+                return channel;
+            } else {
+                return v;
+            }
+        });
+        MeshServiceGrpc.MeshServiceStub stub = MeshServiceGrpc.newStub(ch);
+        while (true) {
+            CountDownLatch latch = new CountDownLatch(1);
+            stub.listen(request, new StreamObserver<PushMsg>() {
+                @Override
+                public void onNext(PushMsg value) {
+                    System.out.println(value);
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    System.out.println("onError:" + t);
+                    latch.countDown();
+                }
+
+                @Override
+                public void onCompleted() {
+                    System.out.println("completed");
+                    latch.countDown();
+                }
+            });
+            latch.await();
+            TimeUnit.SECONDS.sleep(5);
+        }
+    }
+
+    public void shutdown() {
+        channelMap.values().stream().forEach(it -> {
+            it.shutdown();
+        });
+    }
+
+}
---
+++ jcommon/grpc/src/main/resources/protocol/gen.sh
@@ -0,0 +1,2 @@
+PATH_TO_PLUGIN=/Users/zzy/program/protobuf/bin/protoc-gen-grpc-java-1.34.1-osx-x86_64
+protoc --plugin=protoc-gen-grpc-java=$PATH_TO_PLUGIN  --java_out=../../java/ --grpc-java_out=../../java/  service.proto
