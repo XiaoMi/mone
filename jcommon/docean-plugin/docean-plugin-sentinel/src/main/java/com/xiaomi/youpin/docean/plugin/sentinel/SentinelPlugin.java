c2682f69a6948bd91c4c88094b13ca49d3e4b27f
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2023-03-31 17:38:12 +0800

    update

--- jcommon/docean-plugin/docean-plugin-sentinel/src/main/java/com/xiaomi/youpin/docean/plugin/sentinel/SentinelPlugin.java
+++ jcommon/docean-plugin/docean-plugin-sentinel/src/main/java/com/xiaomi/youpin/docean/plugin/sentinel/SentinelPlugin.java
@@ -17,6 +17,7 @@
 package com.xiaomi.youpin.docean.plugin.sentinel;
 
 import com.alibaba.csp.sentinel.config.SentinelConfig;
+import com.alibaba.csp.sentinel.transport.config.TransportConfig;
 import com.xiaomi.youpin.docean.Aop;
 import com.xiaomi.youpin.docean.Ioc;
 import com.xiaomi.youpin.docean.anno.DOceanPlugin;
@@ -37,7 +38,7 @@ public class SentinelPlugin implements IPlugin {
     @Override
     public void init(Set<? extends Class<?>> classSet, Ioc ioc) {
         Config config = ioc.getBean(Config.class);
-        SentinelConfig.setConfig("csp.sentinel.dashboard.server", config.get("sentinel_console_address", ""));
+        SentinelConfig.setConfig(TransportConfig.CONSOLE_SERVER, config.get("sentinel_console_address", ""));
         SentinelConfig.setConfig(SentinelConfig.NACOS_CONFIG_ADDRESS, config.get("nacos_config_server_addr",""));
         Aop.ins().getInterceptorMap().put(Sentinel.class,new SentinelInterceptor());
     }
