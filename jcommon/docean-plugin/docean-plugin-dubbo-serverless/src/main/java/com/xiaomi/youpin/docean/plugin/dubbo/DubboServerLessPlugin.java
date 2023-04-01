ef31fecd83b678c293c9a539d03a4d8c3910689d
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2023-04-02 00:37:42 +0800

    update pom

--- jcommon/docean-plugin/docean-plugin-dubbo-serverless/src/main/java/com/xiaomi/youpin/docean/plugin/dubbo/DubboServerLessPlugin.java
+++ jcommon/docean-plugin/docean-plugin-dubbo-serverless/src/main/java/com/xiaomi/youpin/docean/plugin/dubbo/DubboServerLessPlugin.java
@@ -37,7 +37,6 @@ import org.apache.dubbo.config.*;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.*;
-import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
