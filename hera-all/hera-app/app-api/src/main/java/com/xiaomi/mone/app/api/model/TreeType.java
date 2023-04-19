0b978b3c99e1229b22552e4afdeadf7e5e5513bf
丁涛 <dingtao@xiaomi.com>
2023-04-19 11:05:09 +0800

    add TreeType const class

---
+++ hera-all/hera-app/app-api/src/main/java/com/xiaomi/mone/app/api/model/TreeType.java
@@ -0,0 +1,11 @@
+package com.xiaomi.mone.app.api.model;
+
+/**
+ * @Description
+ * @Author dingtao
+ * @Date 2023/4/19 11:04 AM
+ */
+public class TreeType {
+
+    public static final int TPC = 1;
+}
