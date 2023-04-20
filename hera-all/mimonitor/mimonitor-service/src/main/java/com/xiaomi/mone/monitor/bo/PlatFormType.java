f29d01e0f4644b12b727ab3b3fa49a5c2bc9ea7e
shanwb <shanwenbang@xiaomi.com>
2023-04-20 17:31:15 +0800

    update platformtype

--- hera-all/mimonitor/mimonitor-service/src/main/java/com/xiaomi/mone/monitor/bo/PlatFormType.java
+++ hera-all/mimonitor/mimonitor-service/src/main/java/com/xiaomi/mone/monitor/bo/PlatFormType.java
@@ -10,16 +10,16 @@ import java.util.List;
  * @date 2021/12/2 5:27 下午
  */
 public enum PlatFormType {
-    open(0,"open","open",PlatForm.open,"开源组",0),
+    open(0,"open","open", PlatForm.open,"开源组",0),
     //china原值为0，待拆分
-    china(110,"china","china",PlatForm.mione,"中国区",0),
-    informationDept(1,"informationDept","china",PlatForm.informationDept,"信息部",1),
-    youpin(2,"youpin","youpin",PlatForm.mione,"有品",0),
-    deployment(6,"deployment","cloud",PlatForm.cloud,"云平台(deployment)",6),
-    mice(7,"mice","cloud",PlatForm.cloud,"云平台(mice)",7),
-    matrix(8,"matrix","cloud",PlatForm.cloud,"云平台(matrix)",8),
-    ocean(9,"ocean","cloud",PlatForm.cloud,"云平台(ocean)",9),
-    neo(10,"neo","cloud",PlatForm.cloud,"云平台(neo)",10),
+    china(110,"china","china", PlatForm.mione,"中国区",0),
+    informationDept(1,"informationDept","china",PlatForm.mit,"信息部",1),
+    youpin(2,"youpin","youpin", PlatForm.mione,"有品",0),
+    deployment(6,"deployment","cloud", PlatForm.miCloud,"云平台(deployment)",6),
+    mice(7,"mice","cloud", PlatForm.miCloud,"云平台(mice)",7),
+    matrix(8,"matrix","cloud", PlatForm.miCloud,"云平台(matrix)",8),
+    ocean(9,"ocean","cloud", PlatForm.miCloud,"云平台(ocean)",9),
+    neo(10,"neo","cloud", PlatForm.miCloud,"云平台(neo)",10),
     ;
 
     private Integer code;
@@ -66,36 +66,36 @@ public enum PlatFormType {
         return desc;
     }
 
-    public static PlatFormType getEnum(Integer code){
-        PlatFormType[] values = PlatFormType.values();
-        for(PlatFormType value : values){
-            if(value.getCode().equals(code)){
-                return value;
-            }
-        }
-        return null;
-    }
+//    public static PlatFormType getEnum(Integer code){
+//        PlatFormType[] values = PlatFormType.values();
+//        for(PlatFormType value : values){
+//            if(value.getCode().equals(code)){
+//                return value;
+//            }
+//        }
+//        return null;
+//    }
 
-    public static String getDescByCode(Integer code){
-        PlatFormType[] values = PlatFormType.values();
-        for(PlatFormType value : values){
-            if(value.getCode().equals(code)){
-                return value.getDesc();
-            }
-        }
-        return null;
-    }
+//    public static String getDescByCode(Integer code){
+//        PlatFormType[] values = PlatFormType.values();
+//        for(PlatFormType value : values){
+//            if(value.getCode().equals(code)){
+//                return value.getDesc();
+//            }
+//        }
+//        return null;
+//    }
 
-    public static String getGrafanaDirByCode(Integer code){
-
-        PlatFormType[] values = PlatFormType.values();
-        for(PlatFormType value : values){
-            if(value.getCode().equals(code)){
-                return value.getGrafanaDir();
-            }
-        }
-        return null;
-    }
+//    public static String getGrafanaDirByCode(Integer code){
+//
+//        PlatFormType[] values = PlatFormType.values();
+//        for(PlatFormType value : values){
+//            if(value.getCode().equals(code)){
+//                return value.getGrafanaDir();
+//            }
+//        }
+//        return null;
+//    }
 
     public static Integer getCodeByGrafanaDir(String grafanaDir){
         PlatFormType[] values = PlatFormType.values();
@@ -107,44 +107,44 @@ public enum PlatFormType {
         return null;
     }
 
-    public static boolean isCodeBlondToPlatForm(Integer code,PlatForm platForm){
+//    public static boolean isCodeBlondToPlatForm(Integer code,PlatForm platForm){
+//
+//        PlatFormType[] values = PlatFormType.values();
+//        for(PlatFormType value : values){
+//            if(value.getCode().equals(code) && value.getPlatForm().equals(platForm)){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
-        PlatFormType[] values = PlatFormType.values();
-        for(PlatFormType value : values){
-            if(value.getCode().equals(code) && value.getPlatForm().equals(platForm)){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static List<Pair> getCodeDescList(){
-        List <Pair> list = new ArrayList<>();
-        PlatFormType[] values = PlatFormType.values();
-        for(PlatFormType value : values){
-            Pair pair = new Pair(value.getCode(),value.getDesc());
-            list.add(pair);
-        }
-        return list;
-    }
+//    public static List<Pair> getCodeDescList(){
+//        List <Pair> list = new ArrayList<>();
+//        PlatFormType[] values = PlatFormType.values();
+//        for(PlatFormType value : values){
+//            Pair pair = new Pair(value.getCode(),value.getDesc());
+//            list.add(pair);
+//        }
+//        return list;
+//    }
 
     //检查是否是合法类型
-    public static Integer getMarketType(Integer code){
-        PlatFormType[] values = PlatFormType.values();
-        for(PlatFormType value : values){
-            if(value.getCode().equals(code)){
-                //通过code 给marketCode
-                return value.getMarketCode();
-            }
-        }
-        return -1;
-    }
+//    public static Integer getMarketType(Integer code){
+//        PlatFormType[] values = PlatFormType.values();
+//        for(PlatFormType value : values){
+//            if(value.getCode().equals(code)){
+//                //通过code 给marketCode
+//                return value.getMarketCode();
+//            }
+//        }
+//        return -1;
+//    }
 
-    public static boolean isCloudPlatForm(Integer code){
-        if(code == null){
-            return false;
-        }
-        return cloudPlatCodes.contains(code);
-    }
+//    public static boolean isCloudPlatForm(Integer code){
+//        if(code == null){
+//            return false;
+//        }
+//        return cloudPlatCodes.contains(code);
+//    }
 
 }
