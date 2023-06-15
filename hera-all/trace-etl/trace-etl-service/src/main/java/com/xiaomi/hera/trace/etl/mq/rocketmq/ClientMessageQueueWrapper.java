1f9344830d8d7837f937c2198c2ceeadb65318be
dingtao <dingtao@xiaomi.com>
2023-06-15 11:08:58 +0800

    fix RocketMQ multi-broker Exception

--- hera-all/trace-etl/trace-etl-service/src/main/java/com/xiaomi/hera/trace/etl/mq/rocketmq/ClientMessageQueueWrapper.java
+++ hera-all/trace-etl/trace-etl-service/src/main/java/com/xiaomi/hera/trace/etl/mq/rocketmq/ClientMessageQueueWrapper.java
@@ -1,24 +1,40 @@
 package com.xiaomi.hera.trace.etl.mq.rocketmq;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.common.message.MessageQueue;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @Description
  * @Author dingtao
  * @Date 2022/11/6 5:24 下午
  */
+@Slf4j
 public class ClientMessageQueueWrapper {
 
-    public MessageQueue rocketMQMessageQueue;
+    private MessageQueue rocketMQMessageQueue;
 
-    public BlockingQueue<MessageExt> clientMessageQueue;
+    private BlockingQueue<MessageExt> clientMessageQueue;
 
-    public ClientMessageQueueWrapper(MessageQueue rocketMQMessageQueue, BlockingQueue<MessageExt> clientMessageQueue){
+    private RocketMqProducer producer;
+
+    private ExecutorService executor;
+
+    private boolean running = true;
+
+    public ClientMessageQueueWrapper(MessageQueue rocketMQMessageQueue, BlockingQueue<MessageExt> clientMessageQueue, RocketMqProducer producer) {
         this.rocketMQMessageQueue = rocketMQMessageQueue;
         this.clientMessageQueue = clientMessageQueue;
+        this.producer = producer;
+        startExport();
+        log.info(rocketMQMessageQueue.getBrokerName() + " - "+rocketMQMessageQueue.getQueueId()+" start");
     }
 
     public BlockingQueue<MessageExt> getClientMessageQueue() {
@@ -36,4 +52,86 @@ public class ClientMessageQueueWrapper {
     public void setRocketMQMessageQueue(MessageQueue rocketMQMessageQueue) {
         this.rocketMQMessageQueue = rocketMQMessageQueue;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if(o instanceof ClientMessageQueueWrapper) {
+            ClientMessageQueueWrapper that = (ClientMessageQueueWrapper) o;
+            return Objects.equals(rocketMQMessageQueue, that.rocketMQMessageQueue);
+        }else if (o instanceof MessageQueue){
+            MessageQueue queue = (MessageQueue) o;
+            return Objects.equals(rocketMQMessageQueue, queue);
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return rocketMQMessageQueue.hashCode();
+    }
+
+    private void startExport() {
+        // 初始化导出任务，定时定量从client queue中取出消息，发送到mq
+        executor = Executors.newSingleThreadExecutor(r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(false);
+            thread.setName("client-queue-exporter-" + rocketMQMessageQueue.getBrokerName() + "-" + rocketMQMessageQueue.getQueueId());
+            return thread;
+        });
+        executor.submit(new ClientQueueExporter());
+    }
+
+    public void stopExport() {
+        // 等待所有消息发送成功
+        while (clientMessageQueue.size() > 0) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                log.error("sleep error : ", e);
+            }
+        }
+        running = false;
+        // 销毁线程池
+        executor.shutdown();
+        log.info(rocketMQMessageQueue.getBrokerName()+" - "+rocketMQMessageQueue.getQueueId()+" stopped");
+    }
+
+    private class ClientQueueExporter implements Runnable {
+
+        private static final int CLIENT_QUEUE_BATCH_SEND_SIZE = 1000;
+
+        private static final int CLIENT_QUEUE_SEND_GAP = 1000;
+
+        private long lastSendTime = System.currentTimeMillis();
+
+        public ClientQueueExporter() {
+        }
+
+        @Override
+        public void run() {
+            while (running) {
+                try {
+                    int clientQueueSize = clientMessageQueue.size();
+                    if (clientQueueSize > 0) {
+                        if (clientQueueSize >= CLIENT_QUEUE_BATCH_SEND_SIZE || System.currentTimeMillis() - lastSendTime >= CLIENT_QUEUE_SEND_GAP) {
+                            List<MessageExt> list = new ArrayList<>();
+                            clientMessageQueue.drainTo(list);
+                            producer.send(list, rocketMQMessageQueue);
+                        }
+                    }
+                } catch (Throwable t) {
+                    log.error("client queue exporter error : ", t);
+                }
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    log.error("sleep error : ", e);
+                }
+            }
+        }
+    }
+
 }
