29be0bc31c2c502a7cd86365b018007bad549a4d
wtt <1136220284@qq.com>
2023-07-10 19:42:50 +0800

    feat: update parse regex

--- hera-all/hera-log/log-common/src/main/java/com/xiaomi/mone/log/parse/RegexLogParser.java
+++ hera-all/hera-log/log-common/src/main/java/com/xiaomi/mone/log/parse/RegexLogParser.java
@@ -16,7 +16,6 @@
 package com.xiaomi.mone.log.parse;
 
 import com.gliwka.hyperscan.util.PatternFilter;
-import com.google.common.collect.Lists;
 import com.xiaomi.mone.log.utils.IndexUtils;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -36,17 +35,18 @@ public class RegexLogParser implements LogParser {
 
     private LogParserData parserData;
     private PatternFilter filter;
+    private Pattern pattern;
 
     public RegexLogParser(LogParserData parserData) {
         this.parserData = parserData;
-        Pattern pattern = Pattern.compile(parserData.getParseScript(), Pattern.MULTILINE);
-        List<Pattern> patterns = Lists.newArrayList();
-        patterns.add(pattern);
-        try {
-            this.filter = new PatternFilter(patterns);
-        } catch (Exception e) {
-            this.filter = null;
-        }
+        pattern = Pattern.compile(parserData.getParseScript(), Pattern.MULTILINE);
+//        List<Pattern> patterns = Lists.newArrayList();
+//        patterns.add(pattern);
+//        try {
+//            this.filter = new PatternFilter(patterns);
+//        } catch (Exception e) {
+//            this.filter = null;
+//        }
     }
 
     @Override
@@ -98,15 +98,13 @@ public class RegexLogParser implements LogParser {
         if (filter == null) {
             throw new Exception("compile failed, empty filter");
         }
-        List<Matcher> matchers = filter.filter(logData);
-        if (matchers.size() > 0) {
-            Matcher matcher = matchers.get(0);
-            if (matcher.find()) {
-                // matcher.groupCount() 获取的是 matcher 对象当前有多少个捕获组，不包括 group(0)，所以 groupCount 与 group(i) 索引是不对应的
-                // group(0) 不支持用户获取，用户使用时，valueList 顺序仍从 0 开始即可
-                for (int i = 1; i <= matcher.groupCount(); i++) {
-                    ret.add(matcher.group(i));
-                }
+//        List<Matcher> matchers = filter.filter(logData);
+        Matcher matcher = pattern.matcher(logData);
+        if (matcher.find()) {
+            // matcher.groupCount() 获取的是 matcher 对象当前有多少个捕获组，不包括 group(0)，所以 groupCount 与 group(i) 索引是不对应的
+            // group(0) 不支持用户获取，用户使用时，valueList 顺序仍从 0 开始即可
+            for (int i = 1; i <= matcher.groupCount(); i++) {
+                ret.add(matcher.group(i));
             }
         }
         return ret;
