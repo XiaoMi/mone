812d3a7ae7cda86e9f96db7abaff64025cfe13f6
wtt <1136220284@qq.com>
2023-08-04 11:48:56 +0800

    feat:MethodInfo getter and setter

--- jcommon/processor/src/main/java/run/mone/processor/bo/MethodInfo.java
+++ jcommon/processor/src/main/java/run/mone/processor/bo/MethodInfo.java
@@ -12,6 +12,22 @@ public class MethodInfo implements Serializable {
 
     private String code;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
     public MethodInfo() {
     }
 
