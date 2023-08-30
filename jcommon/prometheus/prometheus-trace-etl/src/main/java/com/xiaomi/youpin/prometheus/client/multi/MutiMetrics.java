0f7efabfb30873cb1e7df703ff8ae4f5a4a23026
oujinliang <oujinliang@gmail.com>
2023-08-30 20:17:07 +0800

    update multi Metrics util class

--- jcommon/prometheus/prometheus-trace-etl/src/main/java/com/xiaomi/youpin/prometheus/client/multi/MutiMetrics.java
+++ jcommon/prometheus/prometheus-trace-etl/src/main/java/com/xiaomi/youpin/prometheus/client/multi/MutiMetrics.java
@@ -3,6 +3,7 @@ package com.xiaomi.youpin.prometheus.client.multi;
 import com.xiaomi.youpin.prometheus.client.XmCounter;
 import com.xiaomi.youpin.prometheus.client.XmGauge;
 import com.xiaomi.youpin.prometheus.client.XmHistogram;
+import io.prometheus.client.CollectorRegistry;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
@@ -21,6 +22,8 @@ public class MutiMetrics {
     public static final String SERVICE = "service";
     public static final String APPLICATION = "application";
 
+    private CollectorRegistry registry;
+
     public void init(String group,String service) {
         setGroup(group);
         setService(service);
@@ -28,19 +31,24 @@ public class MutiMetrics {
 
 
     public void setGroup(String group) {
-        this.gMetricsMgr.constLabels.put(GROUP, group);
+        this.gMetricsMgr.getConstLabels().put(GROUP, group);
     }
 
     public void setService(String service) {
-        this.gMetricsMgr.constLabels.put(SERVICE, service);
+        this.gMetricsMgr.getConstLabels().put(SERVICE, service);
     }
 
     public static double[] DEFAULT_LATENCY_BUCKETS =
             new double[]{.01, .05, 1, 5, 7.5, 10, 25, 50, 100, 200, 500, 1000,1500,2000,3000,4000,5000};
 
     public MutiMetrics() {
-        gMetricsMgr = new MutiPrometheus();
-        gMetricsMgr.constLabels = new HashMap<>();
+        this.registry = new CollectorRegistry(true);
+        gMetricsMgr = new MutiPrometheus(registry);
+        gMetricsMgr.setConstLabels(new HashMap<>());
+    }
+
+    public CollectorRegistry getRegistry(){
+        return this.registry;
     }
 
     public XmCounter newCounter(String metricName, String... labelNames) {
--- jcommon/prometheus/prometheus-trace-etl/src/main/java/com/xiaomi/youpin/prometheus/client/multi/MutiPrometheus.java
+++ jcommon/prometheus/prometheus-trace-etl/src/main/java/com/xiaomi/youpin/prometheus/client/multi/MutiPrometheus.java
@@ -1,6 +1,5 @@
 package com.xiaomi.youpin.prometheus.client.multi;
 
-import com.xiaomi.youpin.prometheus.client.Metrics;
 import com.xiaomi.youpin.prometheus.client.MetricsManager;
 import com.xiaomi.youpin.prometheus.client.PrometheusCounter;
 import com.xiaomi.youpin.prometheus.client.PrometheusGauge;
@@ -8,6 +7,7 @@ import com.xiaomi.youpin.prometheus.client.PrometheusHistogram;
 import com.xiaomi.youpin.prometheus.client.XmCounter;
 import com.xiaomi.youpin.prometheus.client.XmGauge;
 import com.xiaomi.youpin.prometheus.client.XmHistogram;
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
 import io.prometheus.client.Gauge;
 import io.prometheus.client.Histogram;
@@ -25,149 +25,173 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public class MutiPrometheus implements MetricsManager {
 
-   public static final int CONST_LABELS_NUM = 2;
-   public Map<String, String> constLabels;
-   public Map<String, Object> prometheusMetrics;
-   public Map<String,Object> prometheusTypeMetrics;
+    public static final int CONST_LABELS_NUM = 2;
+    private Map<String, String> constLabels;
+    private Map<String, Object> prometheusMetrics;
+    private Map<String, Object> prometheusTypeMetrics;
 
-   private byte[] lock = new byte[0];
-   private byte[] typeLock = new byte[0];
+    private CollectorRegistry registry;
 
-   public MutiPrometheus() {
-      this.prometheusMetrics = new ConcurrentHashMap<>();
-      this.prometheusTypeMetrics = new ConcurrentHashMap<>();
-   }
+    private byte[] lock = new byte[0];
+    private byte[] typeLock = new byte[0];
 
-   @Override
-   public XmCounter newCounter(String metricName, String... labelName) {
-      if (prometheusTypeMetrics.containsKey(metricName)) {
-         return (XmCounter) prometheusTypeMetrics.get(metricName);
-      }
-      synchronized (typeLock) {
-         PrometheusCounter prometheusCounter = new PrometheusCounter(getCounter(metricName, labelName), labelName, null);
-         prometheusTypeMetrics.put(metricName,prometheusCounter);
-         return prometheusCounter;
-      }
-   }
+    public MutiPrometheus() {
+        this.prometheusMetrics = new ConcurrentHashMap<>();
+        this.prometheusTypeMetrics = new ConcurrentHashMap<>();
+    }
 
-   @Override
-   public XmGauge newGauge(String metricName, String... labelName) {
-      if (prometheusTypeMetrics.containsKey(metricName)) {
-         return (XmGauge) prometheusTypeMetrics.get(metricName);
-      }
-      synchronized (typeLock) {
-         PrometheusGauge prometheusGauge = new PrometheusGauge(getGauge(metricName, labelName), labelName, null);
-         prometheusTypeMetrics.put(metricName,prometheusGauge);
-         return prometheusGauge;
-      }
+    public MutiPrometheus(CollectorRegistry registry) {
+        this.prometheusMetrics = new ConcurrentHashMap<>();
+        this.prometheusTypeMetrics = new ConcurrentHashMap<>();
+        this.registry = registry;
+    }
 
-   }
+    public Map<String, String> getConstLabels() {
+        return this.constLabels;
+    }
 
-   @Override
-   public XmHistogram newHistogram(String metricName, double[] bucket, String... labelNames) {
-      if (prometheusTypeMetrics.containsKey(metricName)) {
-         return (XmHistogram) prometheusTypeMetrics.get(metricName);
-      }
-      synchronized (typeLock) {
-         PrometheusHistogram prometheusHistogram = new PrometheusHistogram(getHistogram(metricName, bucket, labelNames), labelNames, null);
-         prometheusTypeMetrics.put(metricName,prometheusHistogram);
-         return prometheusHistogram;
-      }
-   }
+    public Map<String, String> setConstLabels(Map<String, String> constLabels) {
+        return this.constLabels = constLabels;
+    }
 
-   public Counter getCounter(String metricName, String... labelName) {
-      if (constLabels.size() != MutiPrometheus.CONST_LABELS_NUM) {
-         return null;
-      }
-      try {
-         //如果有直接返回
-         if (prometheusMetrics.containsKey(metricName)) {
-            //log.debug("already have metric:" + metricName);
-            return (Counter) prometheusMetrics.get(metricName);
-         }
+    public Map<String, Object> getPrometheusMetrics() {
+        return this.prometheusMetrics;
+    }
 
-         synchronized (lock) {
-            //没有需要先注册一个
-            List<String> mylist = new ArrayList<>(Arrays.asList(labelName));
+    public Map<String, Object> getPrometheusTypeMetrics() {
+        return prometheusTypeMetrics;
+    }
+
+    @Override
+    public XmCounter newCounter(String metricName, String... labelName) {
+        if (prometheusTypeMetrics.containsKey(metricName)) {
+            return (XmCounter) prometheusTypeMetrics.get(metricName);
+        }
+        synchronized (typeLock) {
+            PrometheusCounter prometheusCounter = new PrometheusCounter(getCounter(metricName, labelName), labelName, null, this);
+            prometheusTypeMetrics.put(metricName, prometheusCounter);
+            return prometheusCounter;
+        }
+    }
+
+    @Override
+    public XmGauge newGauge(String metricName, String... labelName) {
+        if (prometheusTypeMetrics.containsKey(metricName)) {
+            return (XmGauge) prometheusTypeMetrics.get(metricName);
+        }
+        synchronized (typeLock) {
+            PrometheusGauge prometheusGauge = new PrometheusGauge(getGauge(metricName, labelName), labelName, null, this);
+            prometheusTypeMetrics.put(metricName, prometheusGauge);
+            return prometheusGauge;
+        }
+
+    }
+
+    @Override
+    public XmHistogram newHistogram(String metricName, double[] bucket, String... labelNames) {
+        if (prometheusTypeMetrics.containsKey(metricName)) {
+            return (XmHistogram) prometheusTypeMetrics.get(metricName);
+        }
+        synchronized (typeLock) {
+            PrometheusHistogram prometheusHistogram = new PrometheusHistogram(getHistogram(metricName, bucket, labelNames), labelNames, null, this);
+            prometheusTypeMetrics.put(metricName, prometheusHistogram);
+            return prometheusHistogram;
+        }
+    }
+
+    public Counter getCounter(String metricName, String... labelName) {
+        if (constLabels.size() != MutiPrometheus.CONST_LABELS_NUM) {
+            return null;
+        }
+        try {
+            //如果有直接返回
+            if (prometheusMetrics.containsKey(metricName)) {
+                //log.debug("already have metric:" + metricName);
+                return (Counter) prometheusMetrics.get(metricName);
+            }
+
+            synchronized (lock) {
+                //没有需要先注册一个
+                List<String> mylist = new ArrayList<>(Arrays.asList(labelName));
 //            mylist.add(Metrics.APPLICATION);
-            String[] finalValue = mylist.toArray(new String[mylist.size()]);
-            Counter newCounter = Counter.build()
-                    .name(metricName)
-                    .namespace(constLabels.get(Metrics.GROUP) + "_" + constLabels.get(Metrics.SERVICE))
-                    .labelNames(finalValue)
-                    .help(metricName)
-                    .register();
+                String[] finalValue = mylist.toArray(new String[mylist.size()]);
+                Counter newCounter = Counter.build()
+                        .name(metricName)
+                        .namespace(constLabels.get(MutiMetrics.GROUP) + ("".equals(constLabels.get(MutiMetrics.SERVICE)) ? "" : "_" + constLabels.get(MutiMetrics.SERVICE)))
+                                .labelNames(finalValue)
+                                .help(metricName)
+                                .register(registry);
 
-            prometheusMetrics.put(metricName, newCounter);
-            return newCounter;
-         }
-      } catch (Throwable throwable) {
-         log.warn(throwable.getMessage());
-         return null;
-      }
-   }
+                prometheusMetrics.put(metricName, newCounter);
+                return newCounter;
+            }
+        } catch (Throwable throwable) {
+            log.warn(throwable.getMessage());
+            return null;
+        }
+    }
 
-   public Gauge getGauge(String metricName, String... labelName) {
-      if (constLabels.size() != MutiPrometheus.CONST_LABELS_NUM) {
-         return null;
-      }
-      try {
-         //如果有直接返回
-         if (prometheusMetrics.containsKey(metricName)) {
-           // log.debug("already have metric:" + metricName);
-            return (Gauge) prometheusMetrics.get(metricName);
-         }
-         synchronized (lock) {
-            //没有需要先注册一个
-            List<String> mylist = new ArrayList<>(Arrays.asList(labelName));
+    public Gauge getGauge(String metricName, String... labelName) {
+        if (constLabels.size() != MutiPrometheus.CONST_LABELS_NUM) {
+            return null;
+        }
+        try {
+            //如果有直接返回
+            if (prometheusMetrics.containsKey(metricName)) {
+                // log.debug("already have metric:" + metricName);
+                return (Gauge) prometheusMetrics.get(metricName);
+            }
+            synchronized (lock) {
+                //没有需要先注册一个
+                List<String> mylist = new ArrayList<>(Arrays.asList(labelName));
 //            mylist.add(Metrics.APPLICATION);
-            String[] finalValue = mylist.toArray(new String[mylist.size()]);
-            Gauge newGauge = Gauge.build()
-                    .name(metricName)
-                    .namespace(constLabels.get(Metrics.GROUP) + "_" + constLabels.get(Metrics.SERVICE))
-                    .labelNames(finalValue)
-                    .help(metricName)
-                    .register();
+                String[] finalValue = mylist.toArray(new String[mylist.size()]);
+                Gauge newGauge = Gauge.build()
+                        .name(metricName)
+                        .namespace(constLabels.get(MutiMetrics.GROUP) + ("".equals(constLabels.get(MutiMetrics.SERVICE)) ? "" : "_" + constLabels.get(MutiMetrics.SERVICE)))
+                        .labelNames(finalValue)
+                        .help(metricName)
+                        .register(registry);
 
-            prometheusMetrics.put(metricName, newGauge);
-            return newGauge;
-         }
-      } catch (Throwable throwable) {
-         log.warn(throwable.getMessage());
-         return null;
-      }
+                prometheusMetrics.put(metricName, newGauge);
+                return newGauge;
+            }
+        } catch (Throwable throwable) {
+            log.warn(throwable.getMessage());
+            return null;
+        }
 
-   }
+    }
 
-   public Histogram getHistogram(String metricName, double[] buckets, String... labelNames) {
-      if (constLabels.size() != MutiPrometheus.CONST_LABELS_NUM) {
-         return null;
-      }
-      try {
-         //如果有直接返回
-         if (prometheusMetrics.containsKey(metricName)) {
-           // log.debug("already have metric:" + metricName);
-            return (Histogram) prometheusMetrics.get(metricName);
-         }
+    public Histogram getHistogram(String metricName, double[] buckets, String... labelNames) {
+        if (constLabels.size() != MutiPrometheus.CONST_LABELS_NUM) {
+            return null;
+        }
+        try {
+            //如果有直接返回
+            if (prometheusMetrics.containsKey(metricName)) {
+                // log.debug("already have metric:" + metricName);
+                return (Histogram) prometheusMetrics.get(metricName);
+            }
 
-         synchronized (lock) {
-            //没有需要注册一个
-            List<String> mylist = new ArrayList<>(Arrays.asList(labelNames));
+            synchronized (lock) {
+                //没有需要注册一个
+                List<String> mylist = new ArrayList<>(Arrays.asList(labelNames));
 //            mylist.add(Metrics.APPLICATION);
-            String[] finalValue = mylist.toArray(new String[mylist.size()]);
-            Histogram newHistogram = Histogram.build()
-                    .buckets(buckets)
-                    .name(metricName)
-                    .namespace(constLabels.get(Metrics.GROUP) + "_" + constLabels.get(Metrics.SERVICE))
-                    .labelNames(finalValue)
-                    .help(metricName)
-                    .register();
-            prometheusMetrics.put(metricName, newHistogram);
-            return newHistogram;
-         }
-      } catch (Throwable throwable) {
-         log.warn(throwable.getMessage());
-         return null;
-      }
-   }
+                String[] finalValue = mylist.toArray(new String[mylist.size()]);
+                Histogram newHistogram = Histogram.build()
+                        .buckets(buckets)
+                        .name(metricName)
+                        .namespace(constLabels.get(MutiMetrics.GROUP) + ("".equals(constLabels.get(MutiMetrics.SERVICE)) ? "" : "_" + constLabels.get(MutiMetrics.SERVICE)))
+                        .labelNames(finalValue)
+                        .help(metricName)
+                        .register(registry);
+                prometheusMetrics.put(metricName, newHistogram);
+                return newHistogram;
+            }
+        } catch (Throwable throwable) {
+            log.warn(throwable.getMessage());
+            return null;
+        }
+    }
 }
