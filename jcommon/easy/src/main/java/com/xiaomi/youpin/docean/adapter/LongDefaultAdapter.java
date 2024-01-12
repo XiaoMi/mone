316a6866269108539e891da4cb8cc812fde7b3f8
wtt <1136220284@qq.com>
2024-01-12 11:19:13 +0800

    feat: Database support for Doris operations

--- jcommon/easy/src/main/java/com/xiaomi/youpin/docean/adapter/LongDefaultAdapter.java
+++ jcommon/easy/src/main/java/com/xiaomi/youpin/docean/adapter/LongDefaultAdapter.java
@@ -1,6 +1,7 @@
 package com.xiaomi.youpin.docean.adapter;
 
 import com.google.gson.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Type;
 
@@ -10,7 +11,10 @@ import java.lang.reflect.Type;
  * @description
  * @date 2021/10/26 14:53
  */
+@Slf4j
 public class LongDefaultAdapter implements JsonSerializer<Long>, JsonDeserializer<Long> {
+
+    private Gson gson = new Gson();
 
     @Override
     public Long deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
@@ -20,6 +24,7 @@ public class LongDefaultAdapter implements JsonSerializer<Long>, JsonDeserialize
         try {
             return json.getAsLong();
         } catch (NumberFormatException e) {
+            log.error("Long LongDefaultAdapter error,param:{}", gson.toJson(json), e);
             throw new JsonSyntaxException(e);
         }
     }
