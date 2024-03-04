078c6a1c950eb353656c020595e98a127e7c7196
张志勇 <zhangzhiyong1@xiaomi.com>
2024-03-04 12:10:26 +0800

    Docean supports JUnit Extension. (#801)
    * update
    
    * Docean supports JUnit Extension.

---
+++ jcommon/docean-plugin/docean-plugin-junit/src/test/java/run/mone/junit/test/TestService.java
@@ -0,0 +1,16 @@
+package run.mone.junit.test;
+
+import com.xiaomi.youpin.docean.anno.Service;
+
+/**
+ * @author goodjava@qq.com
+ * @date 2024/3/3 17:36
+ */
+@Service
+public class TestService {
+
+    public String hi() {
+        return "hello";
+    }
+
+}
