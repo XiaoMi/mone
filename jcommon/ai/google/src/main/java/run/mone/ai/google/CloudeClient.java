0cf16dd7d58230f5a901e399ebd6956641c675f8
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2024-04-12 19:39:12 +0800

    ai support Use local LLM(claude3) close #827

--- jcommon/ai/google/src/main/java/run/mone/ai/google/CloudeClient.java
+++ jcommon/ai/google/src/main/java/run/mone/ai/google/CloudeClient.java
@@ -2,22 +2,28 @@ package run.mone.ai.google;
 
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import lombok.Data;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
+import run.mone.ai.google.bo.Content;
 import run.mone.ai.google.bo.RequestPayload;
 import run.mone.ai.google.bo.ResponsePayload;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author goodjava@qq.com
  * @date 2024/4/9 15:59
  */
 @Data
+@Slf4j
 public class CloudeClient {
 
     private String url = "https://us-central1-aiplatform.googleapis.com/v1/projects/";
@@ -29,6 +35,8 @@ public class CloudeClient {
     private String model = "claude-3-haiku@20240307";
 
     private String token;
+
+    private static Gson gson = new Gson();
 
 
     @SneakyThrows
@@ -46,7 +54,7 @@ public class CloudeClient {
 
 
     public ResponsePayload call(String token, RequestPayload requestPayload) {
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient.Builder().readTimeout(5, TimeUnit.MINUTES).build();
         MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
         RequestBody body = RequestBody.create(mediaType, new Gson().toJson(requestPayload));
         Request request = new Request.Builder()
@@ -57,11 +65,21 @@ public class CloudeClient {
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
+            if (response.code() == 429) {
+                ResponsePayload res = new ResponsePayload();
+                Content content = new Content();
+                content.setText(gson.toJson(ImmutableMap.of("message", "被claude3限流了", "code", "429")));
+                log.info("claude res:{}", content.getText());
+                res.setContent(Lists.newArrayList(content));
+                return res;
+            }
             if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
             // Handle the response
-            return new Gson().fromJson(response.body().string(), ResponsePayload.class);
-        } catch (Exception e) {
-            e.printStackTrace();
+            String res = response.body().string();
+            log.info("claude3 res:{}", res);
+            return new Gson().fromJson(res, ResponsePayload.class);
+        } catch (Throwable e) {
+            log.error(e.getMessage(), e);
         }
         return null;
     }
