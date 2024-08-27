5303918a15a5300f3d6cde1d4d6e8553fc2e14fc
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2024-08-27 14:44:44 +0800

    Code Map Update Test

--- jcommon/ai/neo4j/src/test/java/run/mone/neo4j/test/MoneCodeParserTest.java
+++ jcommon/ai/neo4j/src/test/java/run/mone/neo4j/test/MoneCodeParserTest.java
@@ -43,6 +43,8 @@ public class MoneCodeParserTest {
 //        MoneCodeParser.writeJavaFilesToNeo4j("/Users/zhangzhiyong/IdeaProjects/ai/m78/m78-service/src/main/java/run/mone/m78/service/database/SqlParseUtil.java");
 //        new MoneCodeParser().writeJavaFilesToNeo4j("/Users/zhangzhiyong/IdeaProjects/goodjava/mone/jcommon/ai/neo4j/src/test/java/run/mone/neo4j/test/A.java");
         new MoneCodeParser().setPassword(System.getenv("password")).writeJavaFilesToNeo4j("/Users/zhangzhiyong/IdeaProjects/goodjava/mone/jcommon/ai/neo4j/src/test/java/run/mone/neo4j/test/m");
+//        new MoneCodeParser().setPassword(System.getenv("password")).writeJavaFilesToNeo4j("/Users/zhangzhiyong/IdeaProjects/goodjava/mone/jcommon/ai/neo4j/src/test/java/run/mone/neo4j/test/m/CatService.java");
+//        new MoneCodeParser().setPassword(System.getenv("password")).writeJavaFilesToNeo4j("/Users/zhangzhiyong/IdeaProjects/goodjava/mone/jcommon/ai/neo4j/src/test/java/run/mone/neo4j/test/m/PersonService.java");
     }
 
     @Test
