eacaf1cf57d6e292db576e148eb90e401514688e
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2025-01-14 17:09:06 +0800

    chore: update multiple action and role classes without functional changes

--- jcommon/hive/src/main/java/run/mone/hive/actions/docker/DockerAction.java
+++ jcommon/hive/src/main/java/run/mone/hive/actions/docker/DockerAction.java
@@ -4,9 +4,11 @@ package run.mone.hive.actions.docker;
 import com.google.common.collect.ImmutableMap;
 import run.mone.hive.actions.Action;
 import run.mone.hive.common.AiTemplate;
+import run.mone.hive.common.Prompts;
 import run.mone.hive.schema.ActionContext;
 import run.mone.hive.schema.ActionReq;
 import run.mone.hive.schema.Message;
+import run.mone.hive.utils.XmlParser;
 
 public class DockerAction extends Action {
 
@@ -18,14 +20,15 @@ public class DockerAction extends Action {
     private Message executeDockerCommand(ActionReq req, Action action, ActionContext context) {
         String dockerCommand = "请根据以下要求生成相应的Docker命令:\n" +
                 "${requirements}\n\n" +
-                "请返回完整的Docker命令，无需执行。如果需要多个命令，请用分号分隔。";
+                "请返回完整的Docker命令，无需执行。如果需要多个命令，请用分号分隔。\n" +
+                Prompts.PROMPT_FORMAT;
 
         String prompt = AiTemplate.renderTemplate(dockerCommand, ImmutableMap.of("requirements", req.getMessage().getContent()));
         String res = llm(req).chat(prompt);
-
+        res = XmlParser.trimString(XmlParser.parser(res).get(0));
         return Message.builder()
                 .role(req.getRole().getName())
-                .content("以下是根据您的要求生成的Docker命令：\n\n" + res)
+                .content(res)
                 .build();
     }
 }
--- jcommon/hive/src/main/java/run/mone/hive/roles/Human.java
+++ jcommon/hive/src/main/java/run/mone/hive/roles/Human.java
@@ -2,17 +2,14 @@
 package run.mone.hive.roles;
 
 import run.mone.hive.actions.HumanConfirmAction;
-import run.mone.hive.configs.LLMConfig;
-import run.mone.hive.llm.HumanProvider;
-import run.mone.hive.schema.Message;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 
-public class HumanRole extends Role {
+public class Human extends Role {
 
 
-    public HumanRole(String name, String profile) {
+    public Human(String name, String profile) {
         this.name = name;
         this.profile = profile;
         this.watchList = new HashSet<>();
