d0acad7662a1f7fa34a0687f92715279d1012449
EricDing <128116675+sadadw1@users.noreply.github.com>
2025-01-14 11:04:06 +0800

    fix: add bot http client (#914)
    * fix: add bot http client
    
    * fix: remove default url
    
    * fix: delete unused

--- jcommon/hive/src/main/java/run/mone/hive/roles/DatabaseAssistant.java
+++ jcommon/hive/src/main/java/run/mone/hive/roles/DatabaseAssistant.java
@@ -2,7 +2,6 @@ package run.mone.hive.roles;
 
 import run.mone.hive.actions.Action;
 import run.mone.hive.actions.db.DesignSchemaAction;
-import run.mone.hive.actions.db.GenerateSQLAction;
 import run.mone.hive.actions.db.QueryDataAction;
 import run.mone.hive.actions.db.ModifyDataAction;
 import run.mone.hive.schema.RoleContext;
@@ -37,7 +36,6 @@ public class DatabaseAssistant extends Role {
         // 初始化Actions
         List<Action> actions = Arrays.asList(
                 new DesignSchemaAction(),
-                new GenerateSQLAction(),
                 new QueryDataAction(),
                 new ModifyDataAction()
         );
