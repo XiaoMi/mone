948ac1d93e1bc94fc35f55a8db95098a0763b3d4
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2025-01-17 18:30:40 +0800

    chore: update project configuration and documentation files

---
+++ jcommon/mcp/mpc-redis/src/main/java/run/mone/mpc/redis/server/MongoMcpServer.java
@@ -0,0 +1,54 @@
+package run.mone.mpc.redis.server;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+import run.mone.hive.mcp.server.McpServer;
+import run.mone.hive.mcp.server.McpServer.ToolRegistration;
+import run.mone.hive.mcp.server.McpSyncServer;
+import run.mone.hive.mcp.spec.McpSchema.ServerCapabilities;
+import run.mone.hive.mcp.spec.McpSchema.Tool;
+import run.mone.hive.mcp.spec.ServerMcpTransport;
+import run.mone.mpc.redis.function.RedisFunction;
+
+@Component
+public class MongoMcpServer {
+
+    private ServerMcpTransport transport;
+    private McpSyncServer syncServer;
+
+    public MongoMcpServer(ServerMcpTransport transport) {
+        this.transport = transport;
+    }
+
+    public McpSyncServer start() {
+        McpSyncServer syncServer = McpServer.using(transport)
+                .serverInfo("redis_mcp", "0.0.1")
+                .capabilities(ServerCapabilities.builder()
+                        .tools(true)
+                        .logging()
+                        .build())
+                .sync();
+
+        RedisFunction function = new RedisFunction();
+        var toolRegistration = new ToolRegistration(
+                new Tool(function.getName(), function.getDesc(), function.getToolScheme()), function
+        );
+
+        syncServer.addTool(toolRegistration);
+
+        return syncServer;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.syncServer = start();
+    }
+
+    @PreDestroy
+    public void stop() {
+        if (this.syncServer != null) {
+            this.syncServer.closeGracefully();
+        }
+    }
+}
