a803c348785fea89fee2cac530bf95be0170d228
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2025-02-08 00:13:46 +0800

    chore: update ScrollAction.java without any content changes

--- jcommon/mcp/mcp-playwright/src/main/java/run/mone/mcp/playwright/role/actions/shopper/ScrollAction.java
+++ jcommon/mcp/mcp-playwright/src/main/java/run/mone/mcp/playwright/role/actions/shopper/ScrollAction.java
@@ -13,7 +13,27 @@ public class ScrollAction extends Action {
     public ScrollAction(String description) {
         super("ScrollAction", description);
         setFunction((req,action,ctx)-> Message.builder().content("""
+                 //滚动屏幕
                 <action type="scrollOneScreen">
+                </action>
+                //暂停
+                <action type="pause">
+                </action>
+                
+                <action type="cancelBuildDomTree">
+                </action>
+                
+                <action type="pause">
+                </action>
+                
+                <action type="buildDomTree">
+                </action>
+               
+                <action type="pause">
+                </action>
+                
+                //截图 并且 把截图回传回来
+                <action type="screenshot" send="true" test="true">
                 </action>
                 """).build());
     }
