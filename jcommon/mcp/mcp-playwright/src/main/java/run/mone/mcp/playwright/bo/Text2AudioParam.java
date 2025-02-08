070d59cada1c43eae077c8c165abee5ef164a339
sadadw1 <dingtao@xiaomi.com>
2025-02-08 18:22:54 +0800

    feat: add audio

---
+++ jcommon/mcp/mcp-playwright/src/main/java/run/mone/mcp/playwright/bo/Text2AudioParam.java
@@ -0,0 +1,14 @@
+package run.mone.mcp.playwright.bo;
+
+import lombok.Data;
+
+/**
+ * @author wmin
+ * @date 2024/1/15
+ */
+@Data
+public class Text2AudioParam {
+    private String text;
+    private String dialect;
+    private String voice;
+}
