8e1aa6926de0f35f5cb46616027ce8720d56cb62
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2025-02-11 17:16:53 +0800

    chore: update multiple Java files without content changes

--- jcommon/mcp/mcp-playwright/src/main/java/run/mone/mcp/playwright/role/Searcher.java
+++ jcommon/mcp/mcp-playwright/src/main/java/run/mone/mcp/playwright/role/Searcher.java
@@ -1,14 +1,17 @@
 package run.mone.mcp.playwright.role;
 
+import run.mone.hive.roles.Role;
+
 /**
  * @author goodjava@qq.com
  * @date 2025/2/11 14:54
  */
-public class Searcher extends ChromeOperator{
+public class Searcher extends Role {
 
     public Searcher() {
-        super("Searcher", "网络搜索者");
+        super("Searcher_Chrome工具", "网络搜索者");
         this.goal = """
+                如果用户需求描述中包含购物相关的关键词，例如"查找"、"搜索"、"google"等，请使用搜索相关的工具。
                 搜索步骤:(操作推荐)
                 1.创建google tab(发现没有code的时候,必须调用这个接口)(OpenTabAction)
                 2.在google的页面搜索框里输入要查询的东西(根据用户的需求分析出来),然后点击搜索按钮 (OperationAction)
