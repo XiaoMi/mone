5b0816d6838f6128a5820777008c0820e34acd15
EricDing <128116675+sadadw1@users.noreply.github.com>
2025-02-12 09:35:48 +0800

    fix: distinguish shopper or seacher (#974)
    * feat: add prometheus-trace-etl release
    
    * feat: add gitlab log release jdk21
    
    * feat: update athena
    
    * feat: update check code file path
    
    * feat: add code generator and mybatis generator
    
    * feat: add mybtais generator
    
    * fix: update test error
    
    * fix: add HikariDataSource close
    
    * feat: add chat stream; update session message
    
    * feat: add audio
    
    * feat: add chat flag
    
    * feat: add support multi images
    
    * refactor: shopper rename to chrome athena
    
    * fix: update prompt
    
    * fix: distinguish shopper or seacher

--- jcommon/mcp/mcp-playwright/src/main/java/run/mone/mcp/playwright/role/actions/shopper/OpenTabAction.java
+++ jcommon/mcp/mcp-playwright/src/main/java/run/mone/mcp/playwright/role/actions/shopper/OpenTabAction.java
@@ -2,6 +2,8 @@ package run.mone.mcp.playwright.role.actions.shopper;
 
 import run.mone.hive.actions.Action;
 import run.mone.hive.schema.Message;
+import run.mone.mcp.playwright.common.MultiXmlParser;
+import run.mone.mcp.playwright.common.Result;
 
 /**
  * @author goodjava@qq.com
@@ -11,13 +13,11 @@ public class OpenTabAction extends Action {
 
     public OpenTabAction(String url) {
         setFunction((req, action, ctx) -> {
-            return Message.builder().data(url).content(
-                    """
-                            <action type="createNewTab" url="https://www.jd.com/" auto="true">
-                            打开京东
-                            </action>
-                            """
-            ).build();
+            Message reqMsg = req.getMessage();
+            Result data = (Result) reqMsg.getData();
+            String actions = data.getKeyValuePairs().get("arguments");
+            String xml = new MultiXmlParser().jsonToXml(actions);
+            return Message.builder().data(url).content(xml).build();
         });
     }
 }
