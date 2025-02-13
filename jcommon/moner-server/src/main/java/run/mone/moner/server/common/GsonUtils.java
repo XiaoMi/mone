7bb0676ff8e19b245827d5582e168508fa124873
HawickMason <1914991129@qq.com>
2025-02-13 14:26:46 +0800

    refactor: seperate mcp-playwright and moner-server (#987)
    * feat(mcp-sequential-thinking): add new server
    
    * feat(mcp-sequential-thinking): add log and mod result
    
    * feat(mcp-playwright): add custom selector config
    
    * feat(mcp-playwright): add custom selector config - 1
    
    * feat(mcp-playwright): add custom selector config - backup.js mod
    
    * feat(mcp-playwright): mark element by custom config
    
    * feat(mcp-playwright): add custom config with domain and url
    
    * feat(mcp-playwright): rename mcp-playwright plugin to moner
    
    * feat(mcp-playwright): mod ui
    
    * feat(screenshot): full page capture
    
    * feat(screenshot): full page capture with buildDom customizable
    
    * feat(screenshot): add full page action
    
    * feat(screenshot): add full page action and add mod chrome athena
    
    * feat(mail): add refresh action
    
    * feat(mail): add refresh action -  1
    
    * feat(screen): mod background.js
    
    * feat(screenshot): add download config & deterioration
    
    * refacotr: seperate mcp-playwright and moner-server

---
+++ jcommon/moner-server/src/main/java/run/mone/moner/server/common/GsonUtils.java
@@ -0,0 +1,13 @@
+package run.mone.moner.server.common;
+
+import com.google.gson.Gson;
+
+/**
+ * @author goodjava@qq.com
+ * @date 2024/10/16 12:11
+ */
+public class GsonUtils {
+
+    public static final Gson gson = new Gson();
+
+}
---
+++ jcommon/moner-server/src/main/java/run/mone/moner/server/websocket/WebSocketHandler.java
@@ -0,0 +1,169 @@
+package run.mone.moner.server.websocket;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import run.mone.hive.common.JsonUtils;
+import run.mone.hive.configs.LLMConfig;
+import run.mone.hive.llm.LLM;
+import run.mone.hive.llm.LLMProvider;
+import run.mone.hive.schema.Message;
+import run.mone.moner.server.constant.ResultType;
+import run.mone.moner.server.role.ChromeAthena;
+import run.mone.moner.server.role.actions.GetContentAction;
+import run.mone.moner.server.role.actions.OpenTabAction;
+import run.mone.moner.server.role.actions.OperationAction;
+import run.mone.moner.server.role.actions.ScrollAction;
+import run.mone.moner.server.role.actions.FullPageAction;
+import run.mone.moner.server.role.actions.ClickAfterRefresh;
+import run.mone.moner.server.service.ChromeTestService;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+@Slf4j
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<WebSocketSession>();
+
+    private LLM llm;
+
+    @Resource
+    private ChromeTestService chromeTestService;
+
+    private static final Map<String, ChromeAthena> sessionIdShopper = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        initShopperAndRoleClassifier(session);
+    }
+
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload:{}", payload);
+
+        if (payload.equals("ping") || payload.equals("client_connected")) {
+            return;
+        }
+
+        ChromeAthena chromeAthena = sessionIdShopper.get(session.getId());
+
+        JsonObject req = JsonParser.parseString(payload).getAsJsonObject();
+        String from = JsonUtils.getValueOrDefault(req, "from", "chrome");
+        String cmd = JsonUtils.getValueOrDefault(req, "cmd", "");
+
+        //来自浏览器
+        if (from.equals("chrome")) {
+            JsonObject res = new JsonObject();
+            String data = req.get("data").getAsString();
+
+            if (data.equals("clear")) {
+                log.info("clear");
+                chromeAthena.getRc().getNews().clear();
+                chromeAthena.getRc().getMemory().clear();
+                return;
+            }
+
+            //用来测试
+            if (data.startsWith("?test:")) {
+                session.sendMessage(new TextMessage(chromeTestService.invoke(data.split(":")[1], req, res)));
+                return;
+            }
+
+            if (data.startsWith("!!")) {
+                chromeAthena.getRc().news.put(Message.builder().content(data).build());
+                return;
+            }
+
+            if (cmd.equals("action_ping")) {
+                log.info("action ping");
+                return;
+            }
+
+            //chrome直接返回的(目前只有购物)
+            if (cmd.equals("shopping")) {
+                chromeAthena.getRc().news.put(Message.builder().type("json").content(data).build());
+                return;
+            }
+
+
+            chromeAthena.getRc().news.put(Message.builder().type("json").role("user").content(data).build());
+            new Thread(() -> chromeAthena.run()).start();
+            return;
+        }
+
+        session.sendMessage(new TextMessage(payload));
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        sessionIdShopper.remove(session.getId());
+    }
+
+    public void sendMessageToAll(String message) throws IOException {
+        TextMessage textMessage = new TextMessage(message);
+        for (WebSocketSession session : sessions) {
+            if (session.isOpen()) {
+                session.sendMessage(textMessage);
+            }
+        }
+    }
+
+    private void initShopperAndRoleClassifier(WebSocketSession session) {
+        LLMConfig config = LLMConfig.builder().llmProvider(LLMProvider.GOOGLE_2).build();
+
+        if (config.getLlmProvider() == LLMProvider.GOOGLE_2) {
+            config.setUrl(System.getenv("GOOGLE_AI_GATEWAY") + "streamGenerateContent?alt=sse");
+        }
+
+        if (config.getLlmProvider() == LLMProvider.OPENROUTER && StringUtils.isNotEmpty(System.getenv("OPENROUTER_AI_GATEWAY"))) {
+            config.setUrl(System.getenv("OPENROUTER_AI_GATEWAY"));
+        }
+
+        llm = new LLM(config);
+        ChromeAthena chromeAthena = new ChromeAthena(session);
+        chromeAthena.setLlm(llm);
+        chromeAthena.setActions(
+                //打开页面
+                new OpenTabAction("在tab中打开某个网址"),
+                //点击排名第一的商品
+                new OperationAction(),
+                //点击加入购物车
+                new ScrollAction(),
+                //获取页面内容
+                new GetContentAction(),
+                //全屏截图
+                new FullPageAction(),
+                //刷新页面
+                new ClickAfterRefresh()
+        );
+        chromeAthena.setConsumer(msg -> {
+            try {
+                JsonObject obj = new JsonObject();
+                obj.addProperty("data", msg);
+                obj.addProperty("type", ResultType.ACTION);
+                session.sendMessage(new TextMessage(obj.toString()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+
+        sessionIdShopper.put(session.getId(), chromeAthena);
+    }
+}
