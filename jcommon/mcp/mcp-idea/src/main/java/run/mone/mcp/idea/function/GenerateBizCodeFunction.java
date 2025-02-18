3ca9974ebf2cf1396e9f22597f97fe6e4aa03284
wodiwudi <zhangxiaowei6@xiaomi.com>
2025-02-18 18:54:59 +0800

    refactor: update idea mcp

--- jcommon/mcp/mcp-idea/src/main/java/run/mone/mcp/idea/function/GenerateBizCodeFunction.java
+++ jcommon/mcp/mcp-idea/src/main/java/run/mone/mcp/idea/function/GenerateBizCodeFunction.java
@@ -27,7 +27,22 @@ public class GenerateBizCodeFunction implements Function<Map<String, Object>, Mc
                     "properties": {
                         "requirement": {
                             "type": "string",
+<<<<<<< Updated upstream
                             "description":"需求描述，一定要简短精炼，生成完毕后，不要调用任何MCP工具或者有任何动作"
+=======
+                            "description":"需求描述，用户输入什么就传什么，不要有任何更改，否则会有不好的事情发生"
+                        },
+                        "fileLists": {
+                            "type": "array",
+                            "items": {
+                                "type": "string"
+                            },
+                            "description":"文件列表，根据需求分析出来要操作的文件数组"
+                        },
+                        "projectName": {
+                            "type": "string",
+                            "description":"需要操作的项目，你不应该假设项目名称，如果不知道填什么，请询问用户，否则会有不好的事情发生!"
+>>>>>>> Stashed changes
                         }
                     },
                     "required": ["requirement"]
