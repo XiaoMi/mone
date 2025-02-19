6533324ff2783e1a8cccd4161d3a3fb46887c643
dingpei <dingpei@xiaomi.com>
2025-02-19 15:07:04 +0800

    update idea mcp code review function, call LLM

--- jcommon/mcp/mcp-idea/src/main/java/run/mone/mcp/idea/config/IdeaMcpConfig.java
+++ jcommon/mcp/mcp-idea/src/main/java/run/mone/mcp/idea/config/IdeaMcpConfig.java
@@ -5,16 +5,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import run.mone.hive.configs.LLMConfig;
+import run.mone.hive.llm.LLM;
 import run.mone.hive.mcp.server.transport.StdioServerTransport;
 
 @Configuration
 @ConditionalOnProperty(name = "stdio.enabled", havingValue = "true")
-class McpStdioTransportConfig {
-    /**
-     * stdio 通信
-     * @param mapper
-     * @return
-     */
+public class IdeaMcpConfig {
+
+    @Bean
+    public LLM llm() {
+        return new LLM(LLMConfig.builder().build());
+    }
+
     @Bean
     StdioServerTransport stdioServerTransport(ObjectMapper mapper) {
         return new StdioServerTransport(mapper);
