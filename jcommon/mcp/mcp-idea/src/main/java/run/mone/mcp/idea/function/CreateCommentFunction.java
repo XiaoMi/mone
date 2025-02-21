3e3feb510bccacdf0ee59841378d5ea9d9ae9b17
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2025-02-21 09:46:46 +0800

    chore: update CreateCommentFunction.java and GenerateBizCodeFunction.java

--- jcommon/mcp/mcp-idea/src/main/java/run/mone/mcp/idea/function/CreateCommentFunction.java
+++ jcommon/mcp/mcp-idea/src/main/java/run/mone/mcp/idea/function/CreateCommentFunction.java
@@ -31,14 +31,6 @@ public class CreateCommentFunction implements Function<Map<String, Object>, McpS
             {
                 "type": "object",
                 "properties": {
-                    "projectName": {
-                        "type": "string",
-                        "description": "需要生成注释的项目"
-                    },
-                    "className": {
-                        "type": "string",
-                        "description": "需要操作的class"
-                    },
                     "code": {
                         "type": "string",
                         "description": "The source code that needs to be reviewed"
