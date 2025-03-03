f7d2d95a05a37919cf094865509612c6f92a8d0c
duminchao <duminchao@xiaomi.com>
2025-03-03 09:26:11 +0800

    修改参数描述

--- jcommon/mcp/mcp-moon/src/main/java/run/mone/mcp/moon/MoonBootstrap.java
+++ jcommon/mcp/mcp-moon/src/main/java/run/mone/mcp/moon/MoonBootstrap.java
@@ -1,1 +1,14 @@
- 
+package run.mone.mcp.moon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("run.mone.mcp.moon")
+public class MoonBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonBootstrap.class, args);
+    }
+}
