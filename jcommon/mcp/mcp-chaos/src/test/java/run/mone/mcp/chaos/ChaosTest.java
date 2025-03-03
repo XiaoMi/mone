2d41690f4672b92d55995aacbe8fe6e2f35520f5
wodiwudi <zhangxiaowei6@xiaomi.com>
2025-03-03 17:15:54 +0800

    update test

--- jcommon/mcp/mcp-chaos/src/test/java/run/mone/mcp/chaos/ChaosTest.java
+++ jcommon/mcp/mcp-chaos/src/test/java/run/mone/mcp/chaos/ChaosTest.java
@@ -13,7 +13,7 @@ public class ChaosTest {
     @Test
     public void testGetPipeline() {
         ChaosFunction  chaosFunction = new ChaosFunction();
-        String pipeline = chaosFunction.getPipeline("http://10.38.219.182:80", "zhangxiaowei6", "150918");
+        String pipeline = chaosFunction.getPipeline("http://1.1.1.1:80", "xxx", "1111");
         System.out.println(pipeline);
 
     }
@@ -21,28 +21,28 @@ public class ChaosTest {
     @Test
     public void testGetLists() {
         ChaosFunction chaosFunction = new ChaosFunction();
-        String lists = chaosFunction.getChaosList("http://10.38.219.182:80", "zhangxiaowei6", "150918");
+        String lists = chaosFunction.getChaosList("http://1.1.1.1:80", "xxx", "1111");
         System.out.println(lists);
     }
 
     @Test
     public void testGetDetail() {
         ChaosFunction chaosFunction = new ChaosFunction();
-        String lists = chaosFunction.getChaosDetail("http://10.38.219.182:80", "zhangxiaowei6", "67ad64f1ca15c869d932ec23");
+        String lists = chaosFunction.getChaosDetail("http://1.1.1.1:80", "xxx", "1111");
         System.out.println(lists);
     }
 
     @Test
     public void testExecute() {
         ChaosFunction chaosFunction = new ChaosFunction();
-        String lists = chaosFunction.executeChaos("http://10.38.219.182:80", "zhangxiaowei6", "67c1712f400fb75dfeebe717");
+        String lists = chaosFunction.executeChaos("http://1.1.1.1:80", "xxx", "1111");
         System.out.println(lists);
     }
 
     @Test
     public void testRecover() {
         ChaosFunction chaosFunction = new ChaosFunction();
-        String lists = chaosFunction.recoverChaos("http://10.38.219.182:80", "zhangxiaowei6", "67c1712f400fb75dfeebe717");
+        String lists = chaosFunction.recoverChaos("http://1.1.1.1:80", "xxx", "1111");
         System.out.println(lists);
     }
 
