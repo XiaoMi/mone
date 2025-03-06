808ab60f0ef397ce12ca77ab7434bdae78bc5dc7
HawickMason <1914991129@qq.com>
2025-03-06 14:36:58 +0800

    feat(hive): make react controllable by Role

--- jcommon/hive/src/main/java/run/mone/hive/roles/Role.java
+++ jcommon/hive/src/main/java/run/mone/hive/roles/Role.java
@@ -279,13 +279,22 @@ public class Role {
         //自己决策用那个action
         if (this.rc.getReactMode().equals(RoleContext.ReactMode.REACT)) {
             //需要使用llm来选择action
-            int i = 0;
-            while (this.think() > 0 && i++ < 15) {
-                res = this.act(ac).join();
-            }
+            doReact(ac);
         }
         postReact(ac);
         return CompletableFuture.completedFuture(res);
+    }
+
+    /**
+     * react实际执行的逻辑， 可以重写
+     * @param ac
+     */
+    protected void doReact(ActionContext ac) {
+        // 默认最多执行15次, 可以重写这里的逻辑
+        int i = 0;
+        while (this.think() > 0 && i++ < 15) {
+            this.act(ac).join();
+        }
     }
 
 
@@ -399,7 +408,8 @@ public class Role {
     }
 
     /**
-     * 在react之后执行
+     * 在react之后执行的hook, 可以重写
+     * @param ac
      */
     protected void postReact(ActionContext ac) {
         //子类可以重写此方法
