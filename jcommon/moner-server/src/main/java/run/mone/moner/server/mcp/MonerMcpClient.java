73daf1f68d91979a12c813ef932c195613e3aae9
HawickMason <1914991129@qq.com>
2025-03-14 15:34:40 +0800

    fix(grant): fix grant

--- jcommon/moner-server/src/main/java/run/mone/moner/server/mcp/MonerMcpClient.java
+++ jcommon/moner-server/src/main/java/run/mone/moner/server/mcp/MonerMcpClient.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.mutable.MutableObject;
 
 import lombok.extern.slf4j.Slf4j;
 import run.mone.hive.mcp.spec.McpSchema;
+import run.mone.hive.mcp.spec.McpSchema.Content;
 import run.mone.moner.server.common.GsonUtils;
 import run.mone.moner.server.common.Result;
 import run.mone.moner.server.common.Safe;
