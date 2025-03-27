4cdc04ac82884c52a35d3db19a3ffe6807ca69bd
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2025-03-27 11:09:07 +0800

    feat(mcp-hammerspoon): update sellPutStrategy prompt
    chore(hive): modify HiveConst and WebMvcSseServerTransport

---
+++ jcommon/hive/src/main/java/run/mone/hive/common/HiveConst.java
@@ -0,0 +1,11 @@
+package run.mone.hive.common;
+
+/**
+ * @author goodjava@qq.com
+ * @date 2025/3/27 11:07
+ */
+public class HiveConst {
+
+    public static final String CLIENT_ID = "clientId";
+
+}
