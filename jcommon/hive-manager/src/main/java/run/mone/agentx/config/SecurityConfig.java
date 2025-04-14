113992774c992d14ce63a7af618d090188129c88
HawickMason <1914991129@qq.com>
2025-04-14 16:46:32 +0800

    fix(cycle): cycle-rely-bean fix

--- jcommon/hive-manager/src/main/java/run/mone/agentx/config/SecurityConfig.java
+++ jcommon/hive-manager/src/main/java/run/mone/agentx/config/SecurityConfig.java
@@ -35,9 +35,5 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+   
 }
