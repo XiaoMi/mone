10c09add303e8fbbd54d0988d24b902252eafedc
renqingfu1 <68427470+renqingfu1@users.noreply.github.com>
2025-04-15 10:53:42 +0800

    feat: 新增静态资源 (#1147)

---
+++ jcommon/hive-manager/src/main/resources/static/README.md
@@ -0,0 +1,25 @@
+# Project
+
+## Project Setup
+
+```sh
+pnpm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+pnpm dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+pnpm build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+pnpm lint
+```
---
+++ jcommon/hive-manager/src/main/resources/static/src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import './assets/main.css'
+
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
