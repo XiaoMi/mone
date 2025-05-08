2574afa602ab6c2de53276c5e0df13caf2ca419c
wm <wangmin17@xiaomi.com>
2025-05-08 14:15:20 +0800

    feat(mcp-knowledge-base): hive-spring-starter

--- jcommon/mcp/mcp-knowledge-base/src/main/java/run/mone/mcp/knowledge/base/KnowledgeBaseQueryMcpBootstrap.java
+++ jcommon/mcp/mcp-knowledge-base/src/main/java/run/mone/mcp/knowledge/base/KnowledgeBaseQueryMcpBootstrap.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("run.mone.mcp.knowledge.base")
+@ComponentScan(basePackages = {"run.mone.mcp.knowledge.base", "run.mone.hive.mcp.service"})
 @Slf4j
 public class KnowledgeBaseQueryMcpBootstrap {
     public static void main(String[] args) {
