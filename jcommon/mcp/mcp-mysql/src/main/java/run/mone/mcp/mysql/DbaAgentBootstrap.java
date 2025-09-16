8a11cdcc729fc3439cd31fb156e2bbb05c47c878
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2025-09-16 14:30:19 +0800

    update

--- jcommon/mcp/mcp-mysql/src/main/java/run/mone/mcp/mysql/DbaAgentBootstrap.java
+++ jcommon/mcp/mcp-mysql/src/main/java/run/mone/mcp/mysql/DbaAgentBootstrap.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("run.mone.mcp.mysql")
-public class DatabaseAgentBootstrap {
+public class DbaAgentBootstrap {
     public static void main(String[] args) {
-        SpringApplication.run(DatabaseAgentBootstrap.class, args);
+        SpringApplication.run(DbaAgentBootstrap.class, args);
     }
 }
