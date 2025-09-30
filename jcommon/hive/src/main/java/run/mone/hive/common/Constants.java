ac8dcf94900dd19b32db4e7ef883aeefff99ae90
dingpei <dingpei@xiaomi.com>
2025-09-30 10:00:31 +0800

    fix, update TOKEN_USAGE_LABEL

--- jcommon/hive/src/main/java/run/mone/hive/common/Constants.java
+++ jcommon/hive/src/main/java/run/mone/hive/common/Constants.java
@@ -13,6 +13,6 @@ public class Constants {
 
     public static final String WORKSPACE_PATH = "workspacePath";
 
-    public static final String TOKEN_USAGE_LABEL_START = "\n<chat>\n<usage>\n";
-    public static final String TOKEN_USAGE_LABEL_END = "\n</usage>\n</chat>\n";
+    public static final String TOKEN_USAGE_LABEL_START = "\n<usage>\n";
+    public static final String TOKEN_USAGE_LABEL_END = "\n</usage>\n";
 } 
