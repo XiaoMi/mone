302975a7390fc2772dddb1c4aedcf9bde89ba5af
EricDing <128116675+sadadw1@users.noreply.github.com>
2025-10-11 21:02:39 +0800

    fix: update hera mcp desc (#1406)
    * feat: add cline diff to composer
    
    * fix: add store data mcp
    
    * fix: update store data mcp
    
    * fix: update company claude message deal
    
    * fix: update company claude message deal in BotClient
    
    * feat: add high risk consumer anno
    
    * feat: support multi config url
    
    * feat: add asr and tts tool
    
    * feat: minzai add asr and tts
    
    * feat: convert
    
    * feat: support create role add agentIp parameter
    
    * feat: return finish content, adjust 'max_tokens' state
    
    * fix: update message delta end flag
    
    * fix: add hera analysis
    
    * fix: update custom model
    
    * fix: update default company claude adjust
    
    * fix: add tencent llm
    
    * feat: add MIFY
    
    * feat: action node add send message method
    
    * fix: delete username and password; add token
    
    * fix: 添加webQuery和意图识别
    
    * fix: add release param
    
    * fix: unit test add release param
    
    * feat: tpc add OzHera only data
    
    * feat: update jdk to jdk21; add some field
    
    * fix: update tpc pom
    
    * feat: action node add send begin message
    
    * fix: 添加思考模型关闭思考能力的参数
    
    * fix: 关闭不需要的功能
    
    * feat: hera mcp获取监控指标
    
    * feat: 添加cpu、heap指标数据
    
    * feat: 修改描述

--- jcommon/mcp/mcp-hera-analysis/src/main/java/run/mone/mcp/hera/analysis/config/AgentConfig.java
+++ jcommon/mcp/mcp-hera-analysis/src/main/java/run/mone/mcp/hera/analysis/config/AgentConfig.java
@@ -36,11 +36,11 @@ public class AgentConfig {
     public RoleMeta roleMeta() {
 
         ChatFunction chat = new ChatFunction(agentName, 20);
-        chat.setDesc("");
+        chat.setDesc("和%s聊天，问问%s关于Hera可观测性的服务监控和链路追踪相关的数据。支持各种形式如：'%s'、'请%s告诉我监控数据'、'让%s帮我看看服务状态'、'%s你知道服务有什么问题'等。支持上下文连续对话。");
 
         return RoleMeta.builder()
-                .profile("你是一名优秀的私人助理")
-                .goal("你的目标是更好的帮助用户")
+                .profile("你是Hera可观测系统专家，精通分布式系统的监控和链路追踪，能够帮助用户诊断和解决复杂的系统问题")
+                .goal("你的目标是根据用户输入返回Hera中专业的监控数据和链路追踪数据，帮助用户快速定位和解决系统中的异常和性能问题")
                 .constraints("不要探讨一些负面的东西,如果用户问你,你可以直接拒绝掉")
                 //允许自动从知识库获取内容(意图识别的小模型)
 //                .webQuery(WebQuery.builder().autoWebQuery(true).modelType("bert").version("finetune-bert-20250605-73a29258").releaseServiceName("bert-is-network").build())
