5c96cbc9460f1a94077bf2c7c5b0653c75444c0f
zhangzhiyong <zhangzhiyong1@xiaomi.com>
2025-11-13 21:26:24 +0800

    update

--- jcommon/mcp/mcp-coder/src/main/java/run/mone/mcp/coder/config/AgentConfig.java
+++ jcommon/mcp/mcp-coder/src/main/java/run/mone/mcp/coder/config/AgentConfig.java
@@ -36,45 +36,34 @@ public class AgentConfig {
 
     @Bean
     public RoleMeta roleMeta() {
-        RemoteFileUtils.userKey = userKey;
-        RemoteFileUtils.userSecret = userSecret;
-        RemoteFileUtils.remoteFileApiHost = remoteFileApiHost;
-
-        ListFilesTool listFilesTool = isRemoteFile ? new ListFilesTool(true) : new ListFilesTool(false);
-        ReadFileTool readFileTool = isRemoteFile ? new ReadFileTool(true) : new ReadFileTool(false);
-        WriteToFileTool writeToFileTool = isRemoteFile ? new WriteToFileTool(true) : new WriteToFileTool(false);
-        ReplaceInFileTool replaceInFileTool = isRemoteFile ? new ReplaceInFileTool(true) : new ReplaceInFileTool(false);
-        SearchFilesTool searchFilesTool = isRemoteFile ? new SearchFilesTool(true) : new SearchFilesTool(false);
-
+        initRemoteConfig();
         return RoleMeta.builder()
                 .profile("你是一名优秀的软件工程师")
                 .goal("你的目标是根据用户的需求写好代码")
                 .constraints("不要探讨和代码不想关的东西,如果用户问你,你可以直接拒绝掉")
-                .workflow("有文件修改或写入动作后(比如调用了write_to_file或者replace_in_file 这两个Tool后)，自动触发差异对比(调用DiffTool)")
+//                .workflow("有文件修改或写入动作后(比如调用了write_to_file或者replace_in_file 这两个Tool后)，自动触发差异对比(调用DiffTool)")
                 .tools(Lists.newArrayList(
-                                listFilesTool,
+                                new ListFilesTool(isRemoteFile),
                                 new ExecuteCommandToolOptimized(),
-                                readFileTool,
-                                searchFilesTool,
-                                replaceInFileTool,
+                                new ReadFileTool(isRemoteFile),
+                                new SearchFilesTool(isRemoteFile),
+                                new ReplaceInFileTool(isRemoteFile),
                                 new ListCodeDefinitionNamesTool(),
-                                writeToFileTool,
-                                new DiffTool(),
+                                new WriteToFileTool(isRemoteFile),
+//                                new DiffTool(),
                                 new ChatTool(),
                                 new AskTool(),
                                 new AttemptCompletionTool()
                         )
                 )
                 .mcpTools(Lists.newArrayList(new ChatFunction(agentName, 60)))
-//                .taskList(Lists.newArrayList((role) -> {
-//                    role.putMessage(Message.builder()
-//                            .role("user")
-//                            .content("1+1=?")
-//                            .sink(new McpTransportFluxSink(transport, role))
-//                            .build());
-//                    return "ok";
-//                }))
                 .build();
+    }
+
+    private void initRemoteConfig() {
+        RemoteFileUtils.userKey = userKey;
+        RemoteFileUtils.userSecret = userSecret;
+        RemoteFileUtils.remoteFileApiHost = remoteFileApiHost;
     }
 
 
