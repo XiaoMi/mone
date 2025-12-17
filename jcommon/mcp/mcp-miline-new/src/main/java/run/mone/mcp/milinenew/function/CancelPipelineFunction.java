d8f692f0ae66270eaddbd61de4d27434cc7343c8
李冠辰 <liguanchen@xiaomi.com>
2025-12-17 18:31:18 +0800

    feat: mcp-新增取消流水线的Function和大模型生成jvm参数的Function-2.

--- jcommon/mcp/mcp-miline-new/src/main/java/run/mone/mcp/milinenew/function/CancelPipelineFunction.java
+++ jcommon/mcp/mcp-miline-new/src/main/java/run/mone/mcp/milinenew/function/CancelPipelineFunction.java
@@ -52,7 +52,7 @@ public class CancelPipelineFunction implements McpFunction {
             """;
 
     private static final String BASE_URL = System.getenv("req_base_url");
-    private static final String CANCEL_PIPELINE_URL = BASE_URL != null ? "http://mione-gw.test.mi.com/mtop/miline" + "/cancelPipeline" : null;
+    private static final String CANCEL_PIPELINE_URL = BASE_URL != null ? BASE_URL + "/cancelPipeline" : null;
 
     private final OkHttpClient client;
     private final ObjectMapper objectMapper;
