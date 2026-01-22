f70bd8be3a46343a26672a5032b5d0ee7763f3c8
duminchao <duminchao@xiaomi.com>
2026-01-22 10:22:09 +0800

    添加milinemq消息实体api

--- jcommon/miline-mq-api/src/main/java/com/xiaomi/mione/miline/mq/enums/OperateNodeEventTypeEnum.java
+++ jcommon/miline-mq-api/src/main/java/com/xiaomi/mione/miline/mq/enums/OperateNodeEventTypeEnum.java
@@ -11,36 +11,35 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum OperateNodeEventTypeEnum {
-
     /**
-     * 启动开发
+     * 创建应用变更
      */
-    EVENT_START_CODING(1, "启动开发事件"),
+    EVENT_CREATE_APP_CHANGE("createAppChange", "启动开发事件"),
     /**
-     * 启动部署
+     * 申请应用部署(去部署)
      */
-    EVENT_START_DEPLOY(6, "去部署"),
+    EVENT_CREATE_DEPLOY_REQUEST("createDeployRequest", "去部署"),
 
     /**
      * 完成部署
      */
-    EVENT_DEPLOY_COMPLETE(7, "完成部署"),
+    EVENT_DEPLOY_COMPLETE("deployComplete", "完成部署"),
 
     /**
      * 取消部署
      */
-    EVENT_CANCEL_DEPLOY(8, "取消部署"),
+    EVENT_CANCEL_DEPLOY("cancelDeploy", "取消部署"),
 
     /**
      * 回滚部署
      */
-    EVENT_ROLLBACK_DEPLOY(9, "回滚部署"),
+    EVENT_ROLLBACK_DEPLOY("rollbackDeploy", "回滚部署"),
 
     /**
      * 应用变更状态变更消息
      */
-    EVENT_APP_CHANGE_STATUS(10, "应用变更状态变更消息");
+    EVENT_APP_CHANGE_STATUS("appChangeStatus", "应用变更状态变更消息");
 
-    private final Integer code;
+    private final String tag;
     private final String desc;
 }
